b6e106bb0ea04b70f69ea8a451cac02556e02c7d
Hampuse1 <hampuse1@hotmail.com>
2022-03-23 09:15:11 +0100

    Updates

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "cardsite.db"
+DB_NAME = "school.db"
 
 
 def create_app():
@@ -19,18 +18,11 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Card
+    from .models import Course, Goal
 
     create_database(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
+  
     return app
 
 
--- website/models.py
+++ website/models.py
@@ -1,23 +1,19 @@
 from . import db
-from flask_login import UserMixin
 from sqlalchemy.sql import func
 from flask_appbuilder.models.mixins import ImageColumn
 from flask_appbuilder import Model
 
 
-class Card(db.Model):
+class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10000))
-    rarity = db.Column(db.String(10000))
-    filename = db.Column(db.String(50))
-    price = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    e = db.Column(db.String(10000))
+    c = db.Column(db.String(10000))
+    a = db.Column(db.String(10000))
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
 
-class User(db.Model, UserMixin):
+class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    cards = db.relationship('Card')
-    currency = db.Column(db.Integer)
+    name = db.Column(db.String(150))
+    goals = db.relationship('Goal')
     
